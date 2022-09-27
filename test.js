40c983d174267585dc0c413d6e12df2f73ecfe83
Gabriel Vendramim <101839780+Gvendramim@users.noreply.github.com>
2022-09-26 22:17:59 -0300

    main

---
+++ test.js
@@ -0,0 +1,14 @@
+// PRIMEIRO TESTE
+
+
+// const b = new Barreira(true)
+// b.setAltura(300)
+// document.querySelector('[gv-flappy]').appendChild(b.elemento)
+
+
+// SEGUNDO TESTE
+
+
+// const b = new ParDeBarreiras(700, 200, 800)
+// document.querySelector('[gv-flappy]').appendChild(b.elemento)
+
