548a6179ef7016ecdb514d2752be4e16ef74c12d
Gabriel Vendramim <101839780+Gvendramim@users.noreply.github.com>
2022-09-27 22:03:51 -0300

    main

--- flappy.js
+++ flappy.js
@@ -47,7 +47,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto){
         new ParDeBarreiras (altura, abertura, largura + espaco * 3)
     ]
 
-    const descolamento = 3
+    const descolamento = 5
     this.animar = () => {
         this.pares.forEach(par =>{
             par.setX(par.getX() - descolamento)
@@ -66,10 +66,42 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto){
     }
 }
 
+function Passaro(alturaJogo){
+    let voando = false
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = () => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY () + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0){
+            this.setY(0)
+        }else if (novoY >= alturaMaxima){
+            this.setY(alturaMaxima)
+        }else{
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
 
 const barreiras = new Barreiras (700, 1200, 200, 400)
+const passaro = new Passaro (700)
 const areaDoJogo = document.querySelector('[gv-flappy]')
+
+areaDoJogo.appendChild(passaro.elemento)
 barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
 setInterval(() => {
     barreiras.animar()
+    passaro.animar()
 }, 25)
+
